1d128987b4a72b2312eef28561fc6fd123f63792
psymonn <40018954+psymonn@users.noreply.github.com>
2020-08-20 00:28:51 +1000

    Add files via upload

---
+++ testini.cmd
@@ -0,0 +1,3 @@
+del C:\MININT\SMSOSD\OSDLOGS\VARIABLES.DAT /q
+cscript ZTIGather.wsf /inifile:..\Control\CustomSettings.ini
+"C:\Program Files (x86)\ConfigMgr 2012 Toolkit R2\ClientTools\CMTrace.exe" c:\minint\smsosd\osdlogs\bdd.log
---
+++ testini2.cmd
@@ -0,0 +1,3 @@
+del C:\MININT\SMSOSD\OSDLOGS\VARIABLES.DAT /q
+cscript ZTIGather.wsf /Debug:True /inifile:..\Control\CustomSettings.ini
+notepad.exe c:\minint\smsosd\osdlogs\bdd.log
